66ba27af50db633148166c1132b43f066ace26b0
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-30 00:34:25 -0400

    Move `indexes` to functions.dart

--- range.dart
+++ range.dart
@@ -1,8 +1,5 @@
 import "functions.dart";
 
-Iterable<int> indexes(Iterable<dynamic> input) =>
-  new Iterable<int>.generate(input.length, self);
-
 Iterable<int> range(int start, int end) =>
   new Iterable<int>.generate(end - start + 1, adder(start));
 
