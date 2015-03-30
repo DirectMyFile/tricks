1e7d4088f26e9ca5055d37cfdabe2fc04566feb5
Kenneth Endfinger <kaendfinger@gmail.com>
2015-03-29 21:59:11 -0400

    Add Transpose

---
+++ transpose.dart
@@ -0,0 +1,13 @@
+List<List<dynamic>> transpose(List<List<dynamic>> input) {
+  return input.isEmpty ? [] : new List<int>.generate(input.first.length, (x) => x).map((i) {
+    return input.map((it) => it[i]).toList();
+  }).toList();
+}
+
+void main() {
+  print(transpose([
+    [1, 2],
+    [3, 4],
+    [5, 6]
+  ]));
+}
