8ebe6b980f59d5ce3501003b1efd3c07e7a99f4d
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-11 19:27:13 -0500

    Make note about assert.

--- assert.dart
+++ assert.dart
@@ -1,5 +1,6 @@
 /// Dart has assertions. They are disabled by default.
 /// You can turn them on by running or compiled the script in checked mode.
+/// Assert is a special statement, not a method.
 
 void main() {
   assert("If you see this, then you have assertions enabled" == null);
